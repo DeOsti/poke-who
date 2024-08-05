08f4c832b397abaaf4e91080459eb94c2f5ada3c
Fábio Osti <fabio@deosti.com.br>
2024-08-05 17:52:33 -0300

    Início do projeto

---
+++ assets/js/pokeGetter.js
@@ -0,0 +1,7 @@
+// Criar um valor aleatório entre 1 e 1025
+// Verificar se pokemon com esse ID está salvo localmente
+// Caso sim, usar local. Caso não, Pegar por API e salvar localmente
+
+// Salvar todos os dados em um objeto "pokemon"
+
+// Apenas a entrada de pokedex estará disponível. Todo o resto deverá ser reveladopouco a pouco.
