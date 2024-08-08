2a0cd97fd7f8231a1f7c1d4e8d86601f89c9380a
Fábio Osti <fabio@deosti.com.br>
2024-08-08 13:57:19 -0300

    html e css finalizados

--- assets/js/pokeGetter.js
+++ assets/js/pokeGetter.js
@@ -6,30 +6,9 @@
 
 // Apenas a entrada de pokedex estará disponível. Todo o resto deverá ser reveladopouco a pouco.
 
-function getTypeColor(type) {
-    const typeColor = {
-        normal: "#9FA19F",
-        fire: "#E62829",
-        water: "#2980EF",
-        electric: "#FAC000",
-        grass: "#3FA129",
-        fighting: "#FF8000",
-        poison: "#9141CB",
-        ground: "#915121",
-        flying: "#81B9EF",
-        psychic: "#EF4179",
-        bug: "#91A119",
-        rock: "#AFA981",
-        ghost: "#704170",
-        ice: "#3DCEF3",
-        dragon: "#5060E1",
-        dark: "#624D4E",
-        steel: "#60A1B8",
-        fairy: "#EF70EF",
-        stellar: "#40B5A5",
-    };
-
-    return [typeColor[type], `../images/${type}.png`];
+function getTypeColorClass(type) {
+    return `type-${type}`;
 }
-
-console.log(getTypeColor("normal"));
+function getTypeIcon(type) {
+    return `../images/${type}-icon.png`;
+}
