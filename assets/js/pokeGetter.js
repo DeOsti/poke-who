b8a2a5b993ed55731408d77bc46d6b321397c57c
Fábio Osti <fabio@deosti.com.br>
2024-08-09 00:25:10 -0300

    Finished clicable areas exept change pokemon and reset game

--- assets/js/pokeGetter.js
+++ assets/js/pokeGetter.js
@@ -1,15 +1,57 @@
 // get a ramdom pokemon number between 1 and 1025
 const selectPokemon = Math.ceil(Math.random() * 1025);
 
+async function beginGame() {
+    await setPokemon("1");
+
+    data = JSON.parse(localStorage.getItem("1"));
+    document.querySelector(".pokedex-entry p").textContent = data.dexEntry;
+
+    const clicableFields = document.querySelectorAll(".click");
+    clicableFields.forEach((ele) => {
+        ele.addEventListener("click", () => {
+            const changes = document.getElementById(ele.id);
+
+            switch (ele.id) {
+                case "type1":
+                case "type2":
+                    changes.classList.remove("type-default");
+                    changes.classList.add(`type-${data[ele.id]}`);
+                    changes.querySelector("img").src = `./assets/images/${data[ele.id]}_icon.png`;
+                    changes.querySelector("p").textContent = data[ele.id];
+                    break;
+                case "eggGroup1":
+                case "eggGroup2":
+                case "ability1":
+                case "ability2":
+                case "height":
+                case "weight":
+                case "generation":
+                    changes.querySelector("p").textContent = data[ele.id];
+                    break;
+                case "image":
+                    changes.querySelector("img").src = data.image;
+                    changes.classList.add("poke-hidden");
+                default:
+                    break;
+            }
+        });
+    });
+}
+
+beginGame();
+
+// FUNCTIONS
+
 async function setPokemon(pokemonIndex) {
     // Verify if pokemon info is on local storage
-    if (localStorage.getItem("1")) {
+    if (localStorage.getItem(pokemonIndex)) {
         return;
     }
 
     // fetch pokemon info
-    const pokemon = await getPokemon("1");
-    const pokedex = await getPokedex("1");
+    const pokemon = await getPokemon(pokemonIndex);
+    const pokedex = await getPokedex(pokemonIndex);
 
     // Get random pokedex entry
     const entries = filterDexEntries(pokedex.flavor_text_entries);
@@ -26,18 +68,14 @@ async function setPokemon(pokemonIndex) {
         eggGroup2: pokedex.egg_groups[1].name,
         ability1: pokemon.abilities[0].ability.name,
         ability2: pokemon.abilities[1].ability.name,
-        height: pokemon.height,
-        weight: pokemon.weight,
+        height: +pokemon.height / 10,
+        weight: +pokemon.weight / 10,
         generation: pokedex.generation.name,
     };
 
     // Save in local storage
-    localStorage.setItem("1", JSON.stringify(whoIsThatPokemon));
+    localStorage.setItem(pokemonIndex, JSON.stringify(whoIsThatPokemon));
 }
-
-setPokemon();
-
-// FUNCTIONS
 
 async function getPokemon(pokemonIndex) {
     const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonIndex}`);
